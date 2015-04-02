89373cc1ef6086be4bf005ecd0c005418463b947
terrybroad <terry.m.broad@googlemail.com>
2015-04-02 13:07:02 +0100

    first commit

---
+++ Erode/CMakeFiles/Erode.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Erode.dir/Erode.cpp.o"
+  "Erode.pdb"
+  "Erode"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Erode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ RandomImage/CMakeFiles/RandomImage.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RandomImage.dir/RandomImage.cpp.o"
+  "RandomImage.pdb"
+  "RandomImage"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RandomImage.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
