a7546329c3ec7d1204476ed06f517dac2df9d817
terrybroad <terry.m.broad@googlemail.com>
2015-04-06 22:42:01 +0100

    tex synth experiments 1

---
+++ TexSynth1/CMakeFiles/TexSynth1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TexSynth1.dir/TexSynth1.cpp.o"
+  "TexSynth1.pdb"
+  "TexSynth1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TexSynth1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
